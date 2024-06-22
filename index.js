b68ea82d0dd5a507f889c7a1436e3bc5b61def73
ashiqur12315 <ashiqur12315@gmial.com>
2024-06-22 19:08:03 +0600

    secured routes with verify admin and token

--- index.js
+++ index.js
@@ -89,75 +89,6 @@ async function run() {
 
 
 
-        //___________PAYMENT RELATED API________________//
-
-        // const tran_id = new ObjectId().toString();
-
-        // app.post('/pay', async (req, res) => {
-        //     const jobDetails = req.body;
-        //     const data = {
-        //         total_amount: 100,
-        //         currency: 'BDT',
-        //         tran_id: tran_id, // use unique tran_id for each api call
-        //         success_url: `https://jobjet-server.vercel.app/payment/success/${tran_id}`,
-        //         fail_url: `https://jobjet-server.vercel.app/payment/fail/${tran_id}`,
-        //         cancel_url: 'http://localhost:3030/cancel',
-        //         ipn_url: 'http://localhost:3030/ipn',
-        //         shipping_method: 'Courier',
-        //         product_name: 'Computer.',
-        //         product_category: 'Electronic',
-        //         product_profile: 'general',
-        //         cus_name: 'Customer Name',
-        //         cus_email: 'customer@example.com',
-        //         cus_add1: 'Dhaka',
-        //         cus_add2: 'Dhaka',
-        //         cus_city: 'Dhaka',
-        //         cus_state: 'Dhaka',
-        //         cus_postcode: '1000',
-        //         cus_country: 'Bangladesh',
-        //         cus_phone: '01711111111',
-        //         cus_fax: '01711111111',
-        //         ship_name: 'Customer Name',
-        //         ship_add1: 'Dhaka',
-        //         ship_add2: 'Dhaka',
-        //         ship_city: 'Dhaka',
-        //         ship_state: 'Dhaka',
-        //         ship_postcode: 1000,
-        //         ship_country: 'Bangladesh',
-        //     };
-        //     console.log(data)
-        //     const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-        //     sslcz.init(data).then(apiResponse => {
-        //         // Redirect the user to payment gateway
-        //         let GatewayPageURL = apiResponse.GatewayPageURL
-        //         res.send({ url: GatewayPageURL });
-
-        //         const finalJobDetails = { ...jobDetails, paidStatus: false, transactionId: tran_id }
-        //         console.log(finalJobDetails)
-        //         const result = jobCOllection.insertOne(finalJobDetails)
-
-        //         console.log('Redirecting to: ', GatewayPageURL)
-        //     });
-        //     app.post("/payment/success/:tranId", async (req, res) => {
-        //         console.log(req.params.tranId)
-        //         const result =await jobCOllection.updateOne({transactionId:req.params.tranId},
-        //             {
-        //                 $set: {
-        //                     paidStatus: true
-        //                 }
-        //             }
-        //         )
-        //         if (result.modifiedCount>0){
-        //             res.redirect(`https://a-11-job-marketplace.web.app/payment/success/${req.params.tranId}`)
-        //         }
-        //     })
-        //     app.post("/payment/fail/:tranId", async (req, res)=>{
-        //         const result = await jobCOllection.deleteOne({transactionId: req.params.tranId })
-        //         if(result.deletedCount){
-        //             res.redirect(`https://a-11-job-marketplace.web.app/payment/fail/${req.params.tranId}`)
-        //         }
-        //     })
-        // })
 
         //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -219,7 +150,7 @@ async function run() {
             res.send(result)
         })
 
-        app.patch('/user/:email', async (req, res) => {
+        app.patch('/user/:email', verifyToken, async (req, res) => {
             const email = { email: req.params.email };
             const profileDetails = req.body;
             const updatedDoc = {
@@ -317,7 +248,7 @@ async function run() {
         })
 
         // increase participants count by 1
-        app.patch('/participantCount/:id', async (req, res) => {
+        app.patch('/participantCount/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             const result = await popularCampsCollection.updateOne({ _id: new ObjectId(id) }, {
                 $inc: { participantCount: 1 } // Increment by 1
@@ -349,14 +280,14 @@ async function run() {
 
         });
 
-        app.get('/paymentCampData/:id', async (req, res) => {
+        app.get('/paymentCampData/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await participantsCollection.findOne(query);
             res.send(result);
         })
 
-        app.post('/paymentHistory', async (req, res) => {
+        app.post('/paymentHistory', verifyToken, async (req, res) => {
             const payment = req.body;
             const result = await paymentCollection.insertOne(payment)
             res.send(result)
@@ -406,7 +337,7 @@ async function run() {
 
         // All searching api's
 
-        app.get('/search-payment/:email', async (req, res) => {
+        app.get('/search-payment/:email', verifyToken, async (req, res) => {
             const search = req.query.search;
             const email = req.params.email;
 
